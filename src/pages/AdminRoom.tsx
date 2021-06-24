76a31579dda9d1ddadfbf15f8eb74a169831aeaa
Ilton Barbosa <jiltonbarbosa@gmail.com>
2021-06-24 01:13:00 -0300

    Encerrando uma sala

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import logoImg from '../assets/images/logo.svg';
 import deleteImg from '../assets/images/delete.svg';
 import { Button } from '../components/Button';
@@ -16,8 +16,17 @@ type RoomParams = {
 
 export function AdminRoom() {
 	//const {user} = useAuth();
+	const history = useHistory();
 	const room = useParams<RoomParams>();//pega parâmetros via URL
 	const {title, questions} = useRoom(room.id);
+
+	async function handleEndRoom(){
+		await database.ref(`rooms/${room.id}`).update({
+			endedAt: new Date(),
+		})
+
+		history.push('/');
+	}
 
 	async function handleDeleteQuestion(questionId: string) {
 		if(window.confirm('Tem certeza que deseja excluir esta pergunta?')){
@@ -32,7 +41,10 @@ export function AdminRoom() {
 					<img src={logoImg} alt="LetmeAsk" />
 					<div>
 						<RoomCode code={room.id}/>
-						<Button isOutlined>Encerrar sala</Button>
+						<Button 
+							isOutlined
+							onClick={handleEndRoom}
+							>Encerrar sala</Button>
 					</div>	
 				</div>
 			</header>
